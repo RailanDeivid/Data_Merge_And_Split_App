2f5f10ec9d1611aa9e6e548ec21ddbc96050088e
Railan Deivid <contato.railandeivid@gmail.com>
2024-07-18 09:35:32 -0300

    update

--- app/DataMergeApp.py
+++ app/DataMergeApp.py
@@ -217,7 +217,7 @@ def separar_arquivos():
         elif uploaded_file.name.endswith('.csv'):
             try:
                 # Se o arquivo for CSV, mostra opções para selecionar separador, encoding e coluna
-                cols = st.columns(5)
+                cols = st.columns(4)
                 with cols[0]:
                     sep = st.selectbox("Selecione o separador CSV:", [',', ';'])
                 with cols[1]:
